f19d2e97159a76c5e2c9560e0f67648f498deff9
J Dinesh Reddy <dinesh.reddy@juspay.in>
2026-02-25 10:19:24 +0530

    fix osc52

--- private_dot_config/nvim/lua/plugins/osc-52.lua
+++ private_dot_config/nvim/lua/plugins/osc-52.lua
@@ -1,39 +1,26 @@
 return {
   "ojroques/nvim-osc52",
+  event = "VeryLazy",
   config = function()
     local osc52 = require('osc52')
 
     osc52.setup({
-      silent = false,           -- Display a message on successful copy
-      trim = false,             -- Don't trim surrounding whitespace
-      tmux_passthrough = true,  -- REQUIRED if you use tmux
+      max_length = 0,
+      silent = false,
+      trim = false,
+      tmux_passthrough = false,
     })
 
-    -- Define the copy function using the plugin
-    local function copy(lines, _)
-      osc52.copy(table.concat(lines, '\n'))
-    end
-
-    -- Define a dummy paste function 
-    -- (Note: Most terminals block remote-to-local pasting for security)
-    local function paste()
-      return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
-    end
-
-    -- Sync Neovim's clipboard with the OSC52 provider
-    vim.g.clipboard = {
-      name = 'osc52',
-      copy = {
-        ['+'] = copy,
-        ['*'] = copy,
-      },
-      paste = {
-        ['+'] = paste,
-        ['*'] = paste,
-      },
-    }
-
-    -- THIS IS THE KEY: This makes 'y' use the '+' register automatically
-    vim.opt.clipboard = "unnamedplus"
+    -- Use autocmd to catch ALL yanks
+    vim.api.nvim_create_autocmd('TextYankPost', {
+      callback = function()
+        if vim.v.event.operator == 'y' then
+          local content = vim.fn.getreg('"')
+          if content and content ~= '' then
+            osc52.copy(content)
+          end
+        end
+      end
+    })
   end
 }
