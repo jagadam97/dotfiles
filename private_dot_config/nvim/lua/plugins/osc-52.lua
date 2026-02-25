1be08d6d2ebd794154d72da25f8adacc500e8a36
Dinesh Reddy j <dinesh.reddy@juspay.in>
2026-02-25 09:40:44 +0530

    fix osc-52

--- private_dot_config/nvim/lua/plugins/osc-52.lua
+++ private_dot_config/nvim/lua/plugins/osc-52.lua
@@ -1,25 +1,39 @@
 return {
   "ojroques/nvim-osc52",
   config = function()
-    require('osc52').setup({
-      silent = false,      -- Display a message on successful copy
-      trim = false,        -- Don't trim surrounding whitespace
-      tmux_passthrough = true, -- REQUIRED if you use tmux
+    local osc52 = require('osc52')
+
+    osc52.setup({
+      silent = false,           -- Display a message on successful copy
+      trim = false,             -- Don't trim surrounding whitespace
+      tmux_passthrough = true,  -- REQUIRED if you use tmux
     })
 
-    -- Optional: Use OSC 52 as the default clipboard provider
+    -- Define the copy function using the plugin
     local function copy(lines, _)
-      require('osc52').copy(table.concat(lines, '\n'))
+      osc52.copy(table.concat(lines, '\n'))
     end
 
+    -- Define a dummy paste function 
+    -- (Note: Most terminals block remote-to-local pasting for security)
     local function paste()
       return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
     end
 
+    -- Sync Neovim's clipboard with the OSC52 provider
     vim.g.clipboard = {
       name = 'osc52',
-      copy = {['+'] = copy, ['*'] = copy},
-      paste = {['+'] = paste, ['*'] = paste},
+      copy = {
+        ['+'] = copy,
+        ['*'] = copy,
+      },
+      paste = {
+        ['+'] = paste,
+        ['*'] = paste,
+      },
     }
+
+    -- THIS IS THE KEY: This makes 'y' use the '+' register automatically
+    vim.opt.clipboard = "unnamedplus"
   end
 }
